bf76d456fa4fb5805744dd5c068d477fdfdeeaa7
GCarrell <GCarrell@users.noreply.github.com>
2021-01-08 20:19:17 +0000

    Creation the framework for the Get Recipes request.
    Creation of tests for get recipes request.

--- RecipeAPI_TestingFramework/RecipeAPI_TestingFramework/RecipeConfigReader.cs
+++ RecipeAPI_TestingFramework/RecipeAPI_TestingFramework/RecipeConfigReader.cs
@@ -7,6 +7,6 @@ namespace RecipeAPI_TestingFramework
     {
         public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
         public static readonly string APIKey = ConfigurationManager.AppSettings["api_key"];
-        public static readonly string ApiUrlMod = ConfigurationManager.AppSettings["access_key_url_mod"];
+        public static readonly string ApiUrlMod = ConfigurationManager.AppSettings["x-rapidapi-key"];
     }
 }
--- RecipeAPI_TestingFramework/RecipeAPI_TestingFramework/RecipeGetRecipesService/HTTPManager/GetRecipesCallManager.cs
+++ RecipeAPI_TestingFramework/RecipeAPI_TestingFramework/RecipeGetRecipesService/HTTPManager/GetRecipesCallManager.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using RestSharp;
+
 
 namespace RecipeAPI_TestingFramework
 {
     class GetRecipesCallManager
     {
+        private readonly IRestClient _client;
+        public GetRecipesCallManager()
+        {
+            _client = new RestClient(RecipeConfigReader.BaseUrl);
+        }
+        public string GetRecipes()
+        {
+            var request = new RestRequest("/recipes/extract");
+            request.AddHeader("x-rapidapi-key", RecipeConfigReader.APIKey);
+            request.AddParameter("url", "https://www.melskitchencafe.com/the-best-fudgy-brownies");
+            var response = _client.Get(request);
+
+            return response.Content;
+        }
     }
 }
