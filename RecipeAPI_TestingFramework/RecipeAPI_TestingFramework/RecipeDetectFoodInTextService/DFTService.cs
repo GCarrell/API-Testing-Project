07325620ccf03fcb0fdd40cce5e6b738edd1f480
bibaxhenis <xbxhenis@gmail.com>
2021-01-08 21:38:02 +0000

    Developed Framework so that I can make API requests in order to detect food in text

---
+++ RecipeAPI_TestingFramework/RecipeAPI_TestingFramework/RecipeDetectFoodInTextService/DFTService.cs
@@ -0,0 +1,18 @@
+﻿namespace RecipeAPI_TestingFramework.RecipeDetectFoodInTextService
+{
+    class DFTService
+    {
+        FoodInTextCallManager FoodInTextCallManager { get; set; }
+
+        FoodInTextDTO FoodInTextDTO { get; set; }
+
+        private string DetectedFood { get; set; }
+
+        public DFTService(string requestBody)
+        {
+            DetectedFood = FoodInTextCallManager.GetDetectedFood(requestBody);
+
+            FoodInTextDTO.Desirialize(DetectedFood);
+        }
+    }
+}
--- RecipeAPI_TestingFramework/RecipeAPI_TestingFramework/RecipeDetectFoodInTextService/DataHanding/FoodInTextDTO.cs
+++ RecipeAPI_TestingFramework/RecipeAPI_TestingFramework/RecipeDetectFoodInTextService/DataHanding/FoodInTextDTO.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using Newtonsoft.Json;
 
 namespace RecipeAPI_TestingFramework
 {
     class FoodInTextDTO
     {
+        FoodInTextRoot FoodInTextRoot { get; set; }
+        public void Desirialize(string detectedFood)
+        {
+            FoodInTextRoot = JsonConvert.DeserializeObject<FoodInTextRoot>(detectedFood);
+        }
     }
 }
