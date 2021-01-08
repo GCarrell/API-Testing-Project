7ea0534edb0290391eac112a91f5f104f23157e8
bibaxhenis <xbxhenis@gmail.com>
2021-01-08 13:13:49 +0000

    Initial commit

---
+++ RecipeAPI_TestingFramework/RecipeAPI_TestingFramework/RecipeGetRecipesTests/GetRecipesTest.cs
@@ -0,0 +1,16 @@
+﻿
+using NUnit.Framework;
+using System;
+
+namespace RecipeAPI_TestingFramework
+{
+    
+    public class GetRecipesTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
